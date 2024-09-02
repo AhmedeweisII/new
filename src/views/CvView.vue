b0c50041bef036097f50f74779ec6e6b63a9eb1c
Ahmedeweis <elkaseh95@gmail.com>
2024-09-02 20:41:20 +0300

    add

--- src/views/CvView.vue
+++ src/views/CvView.vue
@@ -454,6 +454,18 @@
             <img src="../assets/img/imgs/gallery-18.png" alt />
           </div>
         </a>
+        <a href="/Logo-generator/authentication.html" class="box" target="_blank">
+          <div class="infoo">
+            <h2>Logo generator</h2>
+            <h2>
+              8
+              <img src="../assets/img/star.png" class="icon2" />
+            </h2>
+          </div>
+          <div class="image">
+            <img src="../assets/img/imgs/gallery-19.png" alt />
+          </div>
+        </a>
         <a href="/Lava/index.html" class="box" target="_blank">
           <div class="infoo">
             <h2>soon</h2>
